122e7eddc51a3af6dd7a61399a502e4a0cfa442f
Atif Niyaz <aniyaz@purdue.edu>
2017-01-23 19:47:44 -0500

    Initial Commit

---
+++ src/com/frc1747/Robot.java
@@ -0,0 +1,44 @@
+package com.frc1747;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+public class Robot extends IterativeRobot {
+
+	@Override
+	public void robotInit() {
+	
+	}
+
+	@Override
+	public void autonomousInit() {
+	
+	}
+	
+	@Override
+	public void disabledInit() {
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+	
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+}
+
