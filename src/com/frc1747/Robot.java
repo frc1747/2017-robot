5777d166e81b966a65aaddd3c55bd2da6740a3ae
Atif Niyaz <aniyaz@purdue.edu>
2017-01-23 20:54:00 -0500

    Solenoid Test

--- src/com/frc1747/Robot.java
+++ src/com/frc1747/Robot.java
@@ -1,14 +1,25 @@
 package com.frc1747;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Robot extends IterativeRobot {
 
+	private Solenoid sol, sol2;
+	
+	
 	@Override
 	public void robotInit() {
-	
+		sol = new Solenoid(0);
+		sol2 = new Solenoid(1);
+	}
+
+	@Override
+	public void teleopInit() {
+		sol.set(true);
+		sol2.set(false);
 	}
 
 	@Override
@@ -18,7 +29,8 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void disabledInit() {
-		
+		sol.set(false);
+		sol2.set(true);
 	}
 
 	@Override
